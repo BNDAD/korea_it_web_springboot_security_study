994b38634add8b3c473e63bbb9db1d154dbc00de
BNDAD <ral7057@naver.com>
2025-06-25 21:54:24 +0900

    Feat: UserRole(수정필요)

---
+++ src/main/java/com/koreait/SpringSecurityStudy/dto/SigninReqDto.java
@@ -0,0 +1,12 @@
+package com.koreait.SpringSecurityStudy.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SigninReqDto {
+    private String username;
+    private String password;
+}
